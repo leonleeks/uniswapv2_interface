733a051adf8a3f412509b26fb9d6b2df345eb035
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-21 10:56:25 -0400

    Update TokenDetail.tsx

--- src/components/Explore/TokenDetail.tsx
+++ src/components/Explore/TokenDetail.tsx
@@ -252,7 +252,7 @@ export default function LoadedTokenDetail({ address }: { address: string }) {
         <AboutSection>
           <AboutHeader>
             <Trans>About</Trans>
-          </AboutHeader>{' '}
+          </AboutHeader>
           {aboutToken}
           <ResourcesContainer>
             <Resource name={'Etherscan'} link={'https://etherscan.io/'} />
